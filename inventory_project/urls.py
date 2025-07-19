d441eb95e748aa0f6229408b0a96a985587511ff
Prempalande123 <prempalande268@gmail.com>
2025-07-19 13:22:31 +0530

    Added API documentation using Swagger and ReDoc

--- inventory_project/urls.py
+++ inventory_project/urls.py
@@ -15,11 +15,30 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+
+# import for api documentation
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# Schema view for api documentation
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Inventory API",
+      default_version='v1',
+      description="API documentation for the Inventory Management System",
+      contact=openapi.Contact(email="your@email.com"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,4 +47,8 @@ urlpatterns = [
     # ✅ JWT Auth endpoints
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+    # for api documentation
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
